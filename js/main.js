d3288531f704e2ac5790c5b2d6ef9a4817ee0908
wayneparker <wayne@wayneparker.biz>
2015-08-03 18:30:01 -0400

    Forgot to stage some files. D'OH!

--- js/main.js
+++ js/main.js
@@ -15,73 +15,16 @@
 //
 // Navigation: Highlight Current Page Links
 
-window.alert('we\'re in');
 var url = location.href.toLowerCase();
-window.alert('URL=' + url);
+console.log('URL=' + url);
 $('#navbar').find('a').each(function () {
-	var link = $(this).href.toLowerCase();
-	window.alert('URL=' + url);
-	if (url.indexOf() > -1) {
-		link.closest("li").addClass('active');
+	var link = this;
+	var str = link.href.toLowerCase();
+	console.log('link=' + str);
+	if (url.indexOf(str) > -1) {
+		$(this).closest("li").addClass('active');
+		console.log('And the winner is: ' + str);
 	}
 });
 
 
-//
-// ## Portfolio
-
-$(document).ready(function() {
-
-	$('.fancybox').fancybox();
-
-	$('.fancybox-thumbs').fancybox({
-		helpers : {
-			thumbs: {
-				width: 100,
-				height: 75
-			}
-		}
-	});
-
-	$('.fancybox-vert').fancybox({
-		helpers : {
-			thumbs: {
-				width: 75,
-				height: 100
-			}
-		}
-	});
-
-	$(".selltoexcel").fancybox({
-		type: 'iframe',
-		width: 1024,
-		height: 660,
-		fitToView: false,
-		autoSize: false,
-		closeClick: false
-	});
-
-	$(".sea-award").fancybox({
-		type: 'swf',
-		width: 720,
-		height: 480,
-		fitToView: false,
-		autoSize: false,
-		closeClick: false,
-		helpers : {
-			thumbs: {
-				width: 100,
-				height: 75
-			}
-		}
-	});
-
-	$(".marquee").fancybox({
-		type: 'swf',
-		width: 630,
-		height: 230,
-		fitToView: false,
-		autoSize: false,
-		closeClick: false
-	});
-});
